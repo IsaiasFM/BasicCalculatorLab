22cc1a5c54b5184c2d604e72758f22488389c8c7
IsaiasFM <59981709+IsaiasFM@users.noreply.github.com>
2020-01-28 17:31:01 -0600

    Adding Heat Index
    Adding in an If statement to add in Heat Index formula.

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -11,29 +11,37 @@ namespace BasicCalculator
             const double num4 = 0.427500;
             const double num5 = 35.7500;
             const double num6 = 35.7400;
+            decimal HeatIndex;
 
             Console.WriteLine("*** BASIC CALCULATOR ***");
 
             Console.WriteLine("Enter the temperature.");
-
-            // int.Parse will take a string data type and convert it to an int data type
             int TempFahr = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the Relative humidity.");
-            int RelHum = int.Parse(Console.ReadLine());
+            if (TempFahr > 50)
+            {
+                Console.WriteLine("Enter the Relative humidity.");
+                int RelHum = int.Parse(Console.ReadLine());
 
-            decimal DewPoint = (TempFahr - num1 * (100 - RelHum));
+                decimal DewPoint = (TempFahr - num1 * (100 - RelHum));
 
-            Console.WriteLine("Dew Point is " + DewPoint);
+                Console.WriteLine("Dew Point is " + DewPoint);
+                HeatIndex = .5m * (TempFahr + 61.0m + ((TempFahr - 68.0m) * 1.2m) + (RelHum * 0.094m));
 
-            Console.WriteLine("Enter the wind speed.");
-            int WindSpeed = int.Parse(Console.ReadLine());
+                Console.WriteLine(HeatIndex);
+            }
 
-            double num2 = ExponCal(WindSpeed);
+            else
+            {
+                Console.WriteLine("Enter the wind speed.");
+                int WindSpeed = int.Parse(Console.ReadLine());
 
-            double WindChill = num6 + (num3 * TempFahr) - (num5 * num2) + (num4 * TempFahr * num2);
+                double num2 = ExponCal(WindSpeed);
 
-            Console.WriteLine("Current wind chill is " + WindChill);
+                double WindChill = num6 + (num3 * TempFahr) - (num5 * num2) + (num4 * TempFahr * num2);
+
+                Console.WriteLine("Current wind chill is " + WindChill);
+            }
 
 
         }
