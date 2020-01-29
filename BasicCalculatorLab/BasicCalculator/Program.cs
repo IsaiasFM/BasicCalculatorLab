cd63f74be64379ee2f55818518b28c559054ed72
IsaiasFM <59981709+IsaiasFM@users.noreply.github.com>
2020-01-29 12:12:20 -0600

    Finalized
    Heat index formula was added.
    Dew point was removed
    While statement was added.

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -4,43 +4,51 @@ namespace BasicCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] arg )
         {
-            const decimal num1 = 0.36m;
             const double num3 = 0.621500;
             const double num4 = 0.427500;
             const double num5 = 35.7500;
             const double num6 = 35.7400;
             decimal HeatIndex;
+            int ContCalc = 1;
 
             Console.WriteLine("*** BASIC CALCULATOR ***");
 
-            Console.WriteLine("Enter the temperature.");
-            int TempFahr = int.Parse(Console.ReadLine());
-
-            if (TempFahr > 50)
+            while (ContCalc == 1)
             {
-                Console.WriteLine("Enter the Relative humidity.");
-                int RelHum = int.Parse(Console.ReadLine());
 
-                decimal DewPoint = (TempFahr - num1 * (100 - RelHum));
+                Console.WriteLine("Enter the temperature.");
+                int TempFahr = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Dew Point is " + DewPoint);
-                HeatIndex = .5m * (TempFahr + 61.0m + ((TempFahr - 68.0m) * 1.2m) + (RelHum * 0.094m));
+                if (TempFahr > 50)
+                {
+                    Console.WriteLine("Enter the Relative humidity.");
+                    int RelHum = int.Parse(Console.ReadLine());
 
-                Console.WriteLine(HeatIndex);
-            }
+                    HeatIndex = .5m * (TempFahr + 61.0m + ((TempFahr - 68.0m) * 1.2m) + (RelHum * 0.094m));
 
-            else
-            {
-                Console.WriteLine("Enter the wind speed.");
-                int WindSpeed = int.Parse(Console.ReadLine());
+                    Console.WriteLine("The heat index is " + HeatIndex);
+                }
 
-                double num2 = ExponCal(WindSpeed);
+                else
+                {
+                    Console.WriteLine("Enter the wind speed.");
+                    int WindSpeed = int.Parse(Console.ReadLine());
 
-                double WindChill = num6 + (num3 * TempFahr) - (num5 * num2) + (num4 * TempFahr * num2);
+                    double num2 = ExponCal(WindSpeed);
 
-                Console.WriteLine("Current wind chill is " + WindChill);
+                    double WindChill = num6 + (num3 * TempFahr) - (num5 * num2) + (num4 * TempFahr * num2);
+
+                    Console.WriteLine("Current wind chill is " + WindChill);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Would you like to calculate again?");
+                Console.WriteLine("1 - yes");
+                Console.WriteLine("2 - no");
+
+                ContCalc = int.Parse(Console.ReadLine());
             }
 
 
